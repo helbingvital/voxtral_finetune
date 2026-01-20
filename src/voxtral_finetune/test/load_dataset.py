a752d3f2d4f46370f6a5ca74530718e87236c390
helbingvital <celina.helbingv@gmail.de>
2026-01-20 15:16:20 +0100

    test data collator

--- src/voxtral_finetune/test/load_dataset.py
+++ src/voxtral_finetune/test/load_dataset.py
@@ -6,10 +6,43 @@ import yaml
 from dataset import _load_radmed_uka, _convert_radmed_uka_split_csv
 from train import train
 from main import load_config
+from collator import VoxtralCollatorTranscriptionTask
+from torch.utils.data import DataLoader
+
+from transformers import (
+    AutoProcessor,
+)
 print("hello")
 
+#----------Load Dataset -------------------------
 # _convert_radmed_uka_split_csv()
-# ds_train, ds_validation = _load_radmed_uka()
+ds_train, ds_validation = _load_radmed_uka(root_path="/hpcwork/ve001107/uka_dataset/")
+# ---------get sample and batch manually -----------------
+first_sample = ds_train[0]
+batch = [ds_train[0],ds_train[1],ds_train[2],ds_train[3]]
 
-cfg = load_config("/Users/helbing/Documents/RWTH/Masterarbeit/voxtral/voxtral-finetune/config/voxtral-3B-mini-debug.yaml")
-train(cfg, "voxtral-3B-mini-debug")
+#---------Set up Data Collator ----------
+model_name = "mistralai/Voxtral-Mini-3B-2507"
+processor = AutoProcessor.from_pretrained(model_name)
+collator = VoxtralCollatorTranscriptionTask(processor=processor, model_id=model_name, language="de")
+
+first_sample_collate = collator([first_sample,])
+batch_feature = collator(batch)
+
+#------- Use DataCollator with Dataloader -------------------
+dataloader = DataLoader(
+    ds_validation, 
+    batch_size=4, 
+    collate_fn=collator,
+    num_workers=2  # Parallel audio decoding
+)
+
+for batch in dataloader:
+    print(len(batch))
+    break
+
+
+#----------Set up Trainer (also loads dataset as specified in config)--------------
+# cfg_path="/home/ve001107/voxtral_finetune/voxtral_finetune/config/voxtral-3B-mini-debug.yaml"
+# cfg = load_config(cfg_path)
+# train(cfg, "voxtral-3B-mini-debug")
