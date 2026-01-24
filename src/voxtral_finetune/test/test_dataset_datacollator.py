45f4602a714dfe61aafe0ea3f030c81376b59428
helbingvital <celina.helbingv@gmail.de>
2026-01-24 16:26:51 +0100

    Rename. Test behaviour of data collator and generation of transcription_request.

--- src/voxtral_finetune/test/test_dataset_datacollator.py
+++ src/voxtral_finetune/test/test_dataset_datacollator.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import yaml
 
-from dataset import _load_radmed_uka, _convert_radmed_uka_split_csv, load_test_data
+from dataset import _load_radmed_uka, _convert_radmed_uka_split_csv, load_test_data, load_train_val_data
 from train import train
 from main import load_config
 from collator import VoxtralCollatorTranscriptionTask
@@ -11,24 +11,36 @@ from torch.utils.data import DataLoader
 
 from transformers import (
     AutoProcessor,
+    VoxtralProcessor
 )
 print("hello")
 
 #----------Load Dataset -------------------------
-# _convert_radmed_uka_split_csv()
-ds_train, ds_validation = _load_radmed_uka(root_path="/hpcwork/ve001107/uka_dataset/")
-df_test = load_test_data({'name': "radmed_uka",
-  'laguage': "de",
-  'root_path': "/Users/helbing/"})
+# _convert_radmed_uka_split_csv(split_csv="./splits/uka_all_20260121.csv", new_split_csv_directory="./splits/uka_all_20260121")
+cfg = load_config("/home/ve001107/voxtral_finetune/voxtral_finetune/config/voxtral-3B-mini-debug.yaml")
+ds_train, ds_validation = load_train_val_data(cfg["dataset"])
+ds_test = load_test_data(cfg["dataset"])
+print(len(ds_train), len(ds_validation), len(ds_test))
+
 # ---------get sample and batch manually -----------------
 first_sample = ds_train[0]
 batch = [ds_train[0],ds_train[1],ds_train[2],ds_train[3]]
 
-#---------Set up Data Collator ----------
+#---------- test apply_transcription_request (inside Collator)-------------
 model_name = "mistralai/Voxtral-Mini-3B-2507"
-processor = AutoProcessor.from_pretrained(model_name)
-collator = VoxtralCollatorTranscriptionTask(processor=processor, model_id=model_name, language="de")
+processor = VoxtralProcessor.from_pretrained(model_name)
+audio_arrays = [item["audio"]["array"] for item in batch]
+tokenized_transcription_request = processor.apply_transcription_request(
+            # language=self.language,
+            audio=first_sample["audio"]["array"],
+            sampling_rate=16000,
+            format=["wav"] * 1,
+            model_id=model_name, 
+            language="de"
+        )
 
+#---------Set up Data Collator ----------
+collator = VoxtralCollatorTranscriptionTask(processor=processor, model_id=model_name, language="de")
 first_sample_collate = collator([first_sample,])
 batch_feature = collator(batch)
 
